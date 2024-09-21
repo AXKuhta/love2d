4e20362e4585473dd8d877a6b80ac957cf97f0b3
AXKuhta <11133304+AXKuhta@users.noreply.github.com>
2024-09-21 14:23:58 +0800

    Fly:update(dt)

--- hello/main.lua
+++ hello/main.lua
@@ -16,18 +16,29 @@ function Fly:create(path, x, y, speed)
 	setmetatable(fly, Fly) -- Объект описывается табличкой
 
 	fly.path = path
+	fly.image = love.graphics.newImage("resources/mosquito_small.png")
 	fly.x = x or 0
 	fly.y = y or 0
+	fly.a_x = 30
+	fly.a_y = 60
 	fly.speed = speed or 1
 
 	return fly
 end
 
+function Fly:update(dt)
+	local rnd_x = love.math.random(0, 3)
+	local rnd_y = love.math.random(0, 3)
 
+	self.x = self.x + self.a_x * dt * rnd_x
+	self.y = self.y + self.a_y * dt * rnd_y
+
+	self.x = self.x % w
+	self.y = self.y % h
+end
 
 function love.load()
 	background = love.graphics.newImage("resources/background.png")
-	fly = love.graphics.newImage("resources/mosquito_small.png")
 
 	w = background:getWidth()
 	h = background:getHeight()
@@ -35,21 +46,16 @@ function love.load()
 	love.window.setMode(w, h)
 
 	love.graphics.setBackgroundColor(.6, .8, 1.)
+
+	fly = Fly:create()
 end
 
 function love.update(dt)
-	local rnd_x = love.math.random(0, 3)
-	local rnd_y = love.math.random(0, 3)
-
-	x = x + a_x * dt * rnd_x
-	y = y + a_y * dt * rnd_y
-
-	x = x % w
-	y = y % h
+	fly:update(dt)
 end
 
 function love.draw()
 	love.graphics.draw(background, 0, 0)
-	love.graphics.draw(fly, x, y)
+	love.graphics.draw(fly.image, fly.x, fly.y)
 	love.graphics.print("FPS: " .. tostring(love.timer.getFPS()), 10, 10)
 end
