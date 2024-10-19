c0db48693cb9639fb8afc963a81e537ad6eaa237
AXKuhta <11133304+AXKuhta@users.noreply.github.com>
2024-10-19 14:59:05 +0800

    Make 10 attractors

--- gravity/main.lua
+++ gravity/main.lua
@@ -47,18 +47,24 @@ function JAM:interact(other)
 end
 ---------------------------------------------------------------------------------------------
 
+jams = {}
+n = 10
+
 function love.load()
 	love.window.setMode(w, h)
 
-	j1 = JAM:create(
-		Vec2:create(200, 200),
-		1
-	)
+	for i=1, n do
+		local x = love.math.random(0, w)
+		local y = love.math.random(0, h)
+		local m = love.math.random(1, 2)
 
-	j2 = JAM:create(
-		Vec2:create(300, 200),
-		2
-	)
+		j = JAM:create(
+			Vec2:create(x, y),
+			m
+		)
+
+		jams[i] = j
+	end
 
 	gravity = Vec2:create(0, 0.01)
 end
@@ -67,17 +73,21 @@ function love.update(dt)
 	x, y = love.mouse.getPosition()
 	v = Vec2:create(x, y)
 
-	j1:update(dt)
-	j2:update(dt)
+	for i=1, n do
+		jams[i]:update(dt)
+	end
 
-	j1:interact(j2)
-	j2:interact(j1)
-
-	--mover_a:apply_force( attr:attract(mover_a) )
-	--mover_b:apply_force( attr:attract(mover_b) )
+	for i=1, n do
+		for j=1, n do
+			if i ~= j then
+			 	jams[i]:interact(jams[j])
+			end
+		end
+	end
 end
 
 function love.draw()
-	j1:draw()
-	j2:draw()
+	for i=1, n do
+		jams[i]:draw()
+	end
 end
